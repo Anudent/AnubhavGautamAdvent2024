7568f4a3c58ea4455361fe9e0d0659071452293f
Anudent <146113253+Anudent@users.noreply.github.com>
2024-12-11 14:06:41 -0500

    Add files via upload

--- DAYTEN_HikingTrailScorer.java
+++ DAYTEN_HikingTrailScorer.java
@@ -5,47 +5,7 @@ import java.util.Scanner;
 
 public class DAYTEN_HikingTrailScorer {
 
-    public int[][] data = formatFileData(getFileData(file));
-
-    public static void main(String[] args){
-
-
-    }
-
-
-
-    public int evaluateTrails(int startX, int startY, int previous){
-        //int totalTrailScore = 0;
-        int x = startX;
-        int y = startY;
-        int currentNumber = data[y][x];
-        if(currentNumber == 0) {
-            if(y-1 > 0) evaluateTrails(x, y+1, currentNumber);
-            if(y+1 < data.length) evaluateTrails(x);
-            if(x-1 > 0) evaluateTrails(x-1, y, currentNumber);
-            if(x+1 < data[0].length ) evaluateTrails(x+1, y, currentNumber);
-
-        }
-
-
-    }
-
-
-    public Array<int> locateAllOneXCoords(String[][] input) {
-        Array<int> allXCoords = new Array<int>();
-        for(int everyRow = 0; everyRow < input.length; everyRow++ ){
-            for(int everyColumn = 0; everyColumn < input[0].length; everyColumn++){
-                if(input[everyRow][everyColumn].equals()) {
-             }
-            }
-        }
-
-    }
-
-    public Array<int> locateAllOneXCoords {}
-
-
-        // turns filedata Array into a 2d list
+    // turns filedata Array into a 2d list
     public int[][] formatFileData(ArrayList<String> fileData){
         int[][] output = new int[fileData.get(0).length()][fileData.size()];   //intializing list to the dimensions of the string's length and the array's size
         for(int eachRow = 0; eachRow < fileData.size(); eachRow++){    //extracting every value in every line
@@ -74,4 +34,63 @@ public class DAYTEN_HikingTrailScorer {
             return fileData;
         }
     }
+
+    //returns all coordinates in the data array that start with 0  in int[] pair
+    public Array<int> locateAllZeroCoords(String[][] input) {
+        Array<int[]> allCoords = new Array<int[]>();
+        for (int everyRow = 0; everyRow < input.length; everyRow++) {
+            for (int everyColumn = 0; everyColumn < input[0].length; everyColumn++) {
+                if (input[everyRow][everyColumn] == 0) {
+                    int[] currentCoord = {everyColumn, everyRow};
+                    allCoords.push(currentCoord);
+                }
+            }
+        }
+        return allCoords;
+    }
+
+    public int[][] data = formatFileData(getFileData(file));
+    public Array<int[]> zeroCoordinates = locateAllOneXCoords(data);
+    public int answer = processAll(zeroCoordinates, data):
+
+    public static void main(String[] args){
+
+
+        system.out.println(answer)
+
+    }
+
+
+
+    public int evaluateTrail(int[] coordinates){
+        //int totalTrailScore = 0;
+        int x = startX;
+        int y = startY;
+        int currentNumber = data[y][x];
+        if(currentNumber != 9) {
+            if(y-1 > 0) evaluateTrails(x, y-1);
+            if(y+1 < data.length) evaluateTrails(x, y+1);
+            if(x-1 > 0) evaluateTrails(x-1, y);
+            if(x+1 < data[0].length ) evaluateTrails(x+1, y);
+
+        }
+    }
+
+    public int processAll(ArrayList<int[]> zeros, int[][] data){
+        int output = 0;
+        for(int[] coordinatePair :  zeros){
+            output += evaluateTrail(coordinatePair);
+        }
+        return output;
+    }
+
+
+
+
+
+
+
+
+
+
 }
