607ebb8fd7503903c9be80efdbfdf4e5af4a489c
Anudent <146113253+Anudent@users.noreply.github.com>
2024-12-19 14:07:51 -0500

    Add files via upload

--- DAYTEN_HikingTrailScorer.java
+++ DAYTEN_HikingTrailScorer.java
@@ -6,6 +6,98 @@ import java.util.Scanner;
 public class DAYTEN_HikingTrailScorer {
 
 
+    public int[][] data = formatFileData(getFileData("src/file.txt"));
+
+    final public static  int[] UP = {1,0};
+    final public static int[] DOWN = {0,0};
+    final public static  int[] LEFT = {0,0};
+    final public static int[] RIGHT = {0,1};
+    final public static  int[][] DIRECTIONS = {UP, DOWN, LEFT, RIGHT};
+
+
+    public static void main(String[] args){
+
+        ArrayList
+                <int[]> zeroCoordinates = locateAllZeroCoords(data);
+        int answer = processAll(zeroCoordinates, data);
+
+        System.out.println(answer);
+
+    }
+
+
+
+    public int evaluateTrail(int[] coordinates, int[][] directions  ){
+
+        int x = coordinates[1];
+        int y = coordinates[0];
+        int total = 0;
+
+        for(int[] direction : directions){
+            int horizontalDirection = direction[1];
+            int verticalDirection = direction[0];
+            int currentNumber = data[y][x];
+
+            if(currentNumber != 9) {
+                if(inBounds(coordinate, DIRECTIONS)){
+                    int nextNumber = data[nextYCoordinate][nextXCoordinate];
+                    if(nextNumber == currentNumber + 1 ){
+                        evaluateTrail(new int[] {nextYCoordinate, nextXCoordinate} , directions);
+                    }
+
+                }
+
+
+
+            }
+            else if( currentNumber >= 9){
+                return 1;
+            }
+        }
+    }
+    
+    public boolean inBounds(int[] coordinates, int[] directions) {
+        int x = coordinates[1];
+        int y = coordinates[0];
+        int horizontalDirection = direction[1];
+        int verticalDirection = direction[0];
+        int nextXCoordinate = x + horizontalDirection;
+        int nextYCoordinate = y + verticalDirection;
+        boolean condition = (0 < nextXCoordinate && nextXCoordinate < data[0].length);
+        condition = condition && (0 < nextYCoordinate && nextYCoordinate < data[0].length);
+
+        return condition;
+
+    }
+        for(int[] direction : directions){
+
+            int x = coordinates[1];
+            int y = coordinates[0];
+            int horizontalDirection = direction[1];
+            int verticalDirection = direction[0];
+            int nextXCoordinate = x + horizontalDirection;
+            int nextYCoordinate = y + verticalDirection;
+
+
+
+        }
+
+
+    }
+
+    public int processAll(ArrayList<int[]> zeros, int[][] data){
+        int output = 0;
+        for(int[] coordinatePair :  zeros){
+
+            evaluateTrailDirection(coordinatePair, );
+        }
+        return output;
+    }
+
+
+
+
+
     // turns filedata Array into a 2d list
     public static int[][] formatFileData(ArrayList<String> fileData){
         int[][] output = new int[fileData.get(0).length()][fileData.size()];   //intializing list to the dimensions of the string's length and the array's size
@@ -37,89 +129,19 @@ public class DAYTEN_HikingTrailScorer {
     }
 
     //returns all coordinates in the data array that start with 0  in int[] pair
-    public static Array<int[]> locateAllZeroCoords(String[][] input) {
-        Array<int[]> allCoords = new Array<int[]>();
+    public static ArrayList<int[]> locateAllZeroCoords(int[][] input) {
+        ArrayList<int[]> allCoords = new ArrayList<int[]>();
         for (int everyRow = 0; everyRow < input.length; everyRow++) {
             for (int everyColumn = 0; everyColumn < input[0].length; everyColumn++) {
                 if (input[everyRow][everyColumn] == 0) {
                     int[] currentCoord = {everyRow, everyColumn};
-                    allCoords.push(currentCoord);
+                    allCoords.add(currentCoord);
                 }
             }
         }
         return allCoords;
     }
 
-    public int[][] data = formatFileData(getFileData("src/file"));
-
-    
-    public static FINAL int[] UP = {1,0};
-    public static FINAL int[] DOWN = {0,0};
-    public static FINAL int[] LEFT = {0,0};
-    public static FINAL int[] RIGHT = {0,1};
-    public static FINAL int[][] DIRECTIONS = {UP, DOWN, LEFT, RIGHT};
-
-    public static void main(String[] args){
-        
-        Array<int[]> zeroCoordinates = locateAllZeroCoords(data);
-        int answer = processAll(zeroCoordinates, data);
-        
-        system.out.println(answer);
-
-    }
-
-
-
-    public int evaluateTrailDirections(int[] coordinates, int[] direction  ){
-        int x = coordinates[1];
-        int y = coordinates[0];
-        int horizontalDirection = direction[1];
-        int verticalDirection = direction[0];
-        int total = 0;
-        int currentNumber = data[y][x];
-        
-        if(currentNumber != 9) {
-            if( inBoundsAndMoveable(coordinates, direction) {
-                
-            } 
-            
-            
-            if(y-1 > 0 && data[y-1][x] == currentNumber + 1 ) {
-                total += evaluateTrailDirections({y-1, x});
-            }
-            if( (y+1 < data.length-1 ) && data[y-1][x] == currentNumber + 1 ) evaluateTrails(x, y+1){
-                total += evaluateTrailDirections({})
-            }
-            if(x-1 > 1) evaluateTrails(x-1, y);
-            if( (x+1 < data[0].length - 1) && && data[y-1][x] == currentNumber + 1 ){
-                total += evaluateTrailDirections()
-            }
-        }
-        else if( currentNumber >= 9){
-            return 1;
-        }
-    }
-
-    public boolean inBoundsAndMoveable(int[] coordinates, int[] direction) {
-        int x = coordinates[1];
-        int y = coordinates[0];
-        int horizontalDirection = direction[1];
-        int verticalDirection = direction[0];
-        boolean condition = ( 0 < x + horizontalDirection && x + horizontalDirection < data[0].length );
-        boolean condition = condition && ( 0 < y + verticalDirection && y + verticalDirection < data[0].length );
-        if(condition) {
-            condition = condition && data[y+ verticalDirection][ x+horizontalDirection ];
-        }
-        return condition;
-    }
-
-    public int processAll(ArrayList<int[]> zeros, int[][] data){
-        int output = 0;
-        for(int[] coordinatePair :  zeros){
-            output += evaluateTrail(coordinatePair);
-        }
-        return output;
-    }
 
 
 
